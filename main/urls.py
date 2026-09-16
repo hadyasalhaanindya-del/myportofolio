afc3c29c4a0a4facb1520a89a8581c1eb8d30736
hadyasalhaanindya-del <hadyasalha.anindya@gmail.com>
2026-09-16 21:09:38 +0700

    Project Form Add

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.http import HttpResponse
 
-from main.views import show_main, show_experience, show_education, show_project, create_project
+from main.views import show_main, show_experience, show_education, show_project, create_project, get_projects_json, delete_project
+
 
 app_name = "main"
 
@@ -10,4 +12,6 @@ urlpatterns = [
     path("education/", show_education, name="show_education"),
     path("project/", show_project, name="show_project"),
     path("project/add/", create_project, name="create_project"),
+    path("api/project/", get_projects_json, name="get_projects_json"),
+    path("project/<uuid:project_id>/delete/",delete_project,name="delete_project")
 ]
--- main/views.py
+++ main/views.py
@@ -41,16 +41,21 @@ def show_education(request):
     return render(request, "education.html", context)
 
 def show_project(request):
+    json_response = get_projects_json(request)
+
+    projects = serializers.deserialize(
+        "json",
+        json_response.content.decode("utf-8"),
+    )
+    projects = [project.object for project in projects]
+    title_query = request.GET.get("title", "").strip()
+
     context = {
         "name": "Hadya",
         "project_list": Project.objects.all(),
+        "title_query": title_query,
     }
     return render(request, "project.html", context)
-
-
-
-
-
 
 def create_project(request):
     form = ProjectForm(request.POST or None)
@@ -66,4 +71,23 @@ def create_project(request):
     }
     return render(request, "projects_form.html", context)
 
-    
+
+def get_projects_json(request):
+    title_query = request.GET.get("title", "").strip()
+    projects = Project.objects.all()
+
+    if title_query:
+        projects = projects.filter(title__icontains=title_query)
+
+    projects_json = serializers.serialize("json", projects)
+    return HttpResponse(projects_json, content_type="application/json")
+
+def delete_project(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+
+    if request.method == "POST":
+        project.delete()
+        messages.success(request, "Project deleted!")
+        return redirect("main:show_project")
+
+    return redirect("main:show_project")
