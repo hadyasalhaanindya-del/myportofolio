5d214cb026f4fa9e1c773328b8bc606dcce5f876
hadyasalhaanindya-del <hadyasalha.anindya@gmail.com>
2026-09-16 17:00:08 +0700

    Project Form

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, show_education, show_project
+from main.views import show_main, show_experience, show_education, show_project, create_project
 
 app_name = "main"
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path("experience/", show_experience, name="show_experience"),
     path("education/", show_education, name="show_education"),
     path("project/", show_project, name="show_project"),
+    path("projects/add/", create_project, name="create_project"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+
+
+
+from django.contrib import messages
+from django.core import serializers
+from django.http import HttpResponse
 
 from main.models import Experience, Education, Project
+from main.forms import ProjectForm
+
 
 
 def show_main(request):
@@ -40,5 +48,22 @@ def show_project(request):
     return render(request, "project.html", context)
 
 
+
+
+
+
+def create_project(request):
+    form = ProjectForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "New Project Added!")
+        return redirect("main:show_project")
+
+    context = {
+        "name": "Hadya",
+        "form": form,
+    }
+    return render(request, "projects_form.html", context)
 
     
